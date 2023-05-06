d4e4966711b2b79cf723c8afe31566fde46eda0a
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-06 22:52:35 +0700

    okey

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3002
+var allowedOrigins = ['http://localhost:3000','https://users-admin.vercel.app'];
 
 try {
     await users_admin.authenticate()
@@ -19,10 +20,19 @@ try {
 
 
 app.use(cookieParser())
+
 app.use(cors({
-    origin : true,
-    credentials : true,
-}))
+  origin: function(origin, callback){    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }    return callback(null, true);
+  },
+  credentials : true
+}));
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(router)
