704bad06fe85271e557f456ff2e4f60d584045b3
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-06 11:55:35 +0700

    dsds

--- controllers/refresh_token.js
+++ controllers/refresh_token.js
@@ -21,7 +21,23 @@ export default async function RefreshToken(req,res) {
         const accesstoken = jwt.sign({nama,id,email},process.env.KEY_ACCESS_TOKEN,{
             expiresIn : '15s'
         })
-     
+          res.setHeader('Access-Control-Allow-Credentials', true)
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  // another common pattern
+
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
+  res.setHeader(
+
+    'Access-Control-Allow-Headers',
+
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+
+  )
         res.json({accesstoken})
     })
   } catch (error) {
