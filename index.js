2e9e80fdb11d87232398053057a429ba34b9fd5d
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-06 23:01:27 +0700

    okeys

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.use(cors({
   credentials : true
 }));
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+    });
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(router)
