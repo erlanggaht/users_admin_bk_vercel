8343e141853d7f137ee9682bff0cee990d6307da
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-06 11:29:40 +0700

    setheader

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import mysql2 from 'mysql2'
 
 const users_admin = new Sequelize('users_admin','erlanggaht','@sumedang12',{
-    host : "www.db4free.net",
+    host : "db4free.net",
     dialect : "mysql",
     dialectModule : mysql2
     })
